cec6abc9ebd18fe2ef836fcb99759ab6676437e7
Cameron Jackson <cameronjackson@Camerons-MBP-2.T-mobile.com>
2017-05-05 13:45:56 -0400

    feat: add tldr generation

--- server.js
+++ server.js
@@ -2,7 +2,10 @@
 
 var express = require('express')
 var app = express()
+const querystring = require("querystring");
 const feeds = require('./feeds')
+const summary = require('node-summary');
+const request = require('request');
 
 let port = (process.env.SERVER_PORT || process.env.PORT || 5000 )
 
@@ -15,6 +18,27 @@ app.get('/', function (req, res) {
     Health: feeds.health(),
     World: feeds.world()
   })
+})
+
+app.get('/tldr', (req, res) => {
+  const url = req.query.url;
+  
+  // const encoded_url = querystring.escape(url);
+
+  var options = {
+    url: "http://api.smmry.com",
+    method: "GET",
+    qs: {
+      'SM_URL': url,
+      'SM_API_KEY': process.env.SMMRY_KEY
+    }
+  }
+
+  request(options, function (error, response, body) {
+    console.log(body);
+    res.send(body);
+  })
+
 })
 
 app.listen(port, function () {
